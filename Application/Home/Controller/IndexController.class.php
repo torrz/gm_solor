6f1a44ada8938d1220bf8523c5c9d8e640b45b3a
hemecity-W <3157254503@qq.com>
2016-01-20 17:06:46 +0800

    调整目录结构

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,6 +4,7 @@ use Think\Controller;
 class IndexController extends Controller {
     public function index(){
         $this->display('login');
+//        $this->success('新增成功', './Home/Customer/index',5);
     }
     public function add(){
         $this->success('新增成功', '../Customer/index',5);
